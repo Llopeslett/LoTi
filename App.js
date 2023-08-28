3de7a59b6a2fa915c4edc250bb2427f68632e5c4
Llopeslett <leticialopesf57@gmail.com>
2023-08-28 10:25:38 -0300

    branch com o código da aula do dia Mon, Aug 28, 2023 10:25:38 AM

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+import TelaLogin from './src/TelaLogin';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{height: '00%'}}>
+      <TelaLogin></TelaLogin>
     </View>
   );
 }

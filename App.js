74ec17139bab49c67dd750180aa6ab2b21eda306
Llopeslett <leticialopesf57@gmail.com>
2023-09-04 10:40:34 -0300

    branch com o código da aula do dia Mon, Sep  4, 2023 10:40:34 AM

---
+++ App.js
@@ -0,0 +1,20 @@
+import {StatusBar } from "expo-status-bar";
+import React from 'react';
+import { SafeAreaView,StyleSheet} from "react-native";
+
+import LoginScreen from './src/LoginScreen';
+
+const App = () => {
+  return (
+      <SafeAreaView>
+        <LoginScreen/>
+      </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1
+    },
+});
+

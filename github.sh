f9e0195d999c9c8341cb2c6a95fc61ffc7e704b9
Llopeslett <leticialopesf57@gmail.com>
2023-09-04 10:45:29 -0300

    branch com o código da aula do dia Mon, Sep  4, 2023 10:45:29 AM

--- github.sh
+++ github.sh
@@ -4,7 +4,7 @@
 #na variável repositório, prefira a url do repositório baseada em https para nao termos que configurar SSH
 export name='Llopeslett'
 export email="leticialopesf57@gmail.com"
-export repositorio="https://github.com/Llopeslett/LoTi.git";
+export repositorio="https://github.com/Llopeslett/04092023.git";
 
 #Daqui pra baixo mexa somente se for realmente necessário ou se souber o que está fazendo :P
 
